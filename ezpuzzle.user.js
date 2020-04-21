13e328f00661fc8d1b67e1c0a91d3a4564bbff0b
Optimus <optimusprimeagario@gmail.com>
2020-04-21 15:36:05 -0400

    Create ezpuzzle.user.js

---
+++ ezpuzzle.user.js
@@ -0,0 +1,62 @@
+// ==UserScript==
+// @name         Edpuzzle Helper
+// @version      1
+// @description  Gets answers for multiple choices!
+// @author       Prime
+// @match        https://edpuzzle.com/assignments/*
+// @grant        none
+// ==/UserScript==
+
+let tries = 0;
+let isSuccess = false;
+let answers = [];
+
+
+function getAssignment() {
+  console.log(`[✔️] got the assignment id`)
+  return new URL(document.URL).pathname.split("/")[2];
+}
+
+
+function sendRequest() {
+  if (tries == 3) {
+    console.log(`[Edpuzzle] something went wrong..`);
+    return;
+  }
+  const edpuzzle = new XMLHttpRequest();
+  edpuzzle.open('GET', `https://edpuzzle.com/api/v3/assignments/${getAssignment()}`, true);
+
+  edpuzzle.onerror = data => {
+    isSuccess = false;
+    tries++;
+    console.log(`[❌] retrying..`);
+    sendRequest();
+  }
+
+  edpuzzle.onload = data => {
+    parseData(edpuzzle.response);
+  }
+
+  edpuzzle.send();
+}
+
+function parseData(unParsed) {
+  let data = JSON.parse(unParsed);
+  console.log(`[✔️] Got the answers! 😉`);
+  data.medias[0].questions.forEach(questions => {
+    if (questions.type.includes("multiple")) {
+      questions.choices.forEach(choice => {
+        if(choice.isCorrect) {
+          console.log(`Q: ${questions.body[0].text}
+A: ${choice.body[0].html}
+          `)
+        }
+      });
+    }
+  });
+}
+
+
+setTimeout(() => {
+  sendRequest();
+}, 2000);
